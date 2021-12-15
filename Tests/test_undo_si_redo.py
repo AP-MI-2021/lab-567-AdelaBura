06e16277a0628ef3689076e90bd93efc0e45434a
AdelaBura <buraadela599@gmail.com>
2021-12-15 18:11:16 +0200

    lab567

--- Tests/test_undo_si_redo.py
+++ Tests/test_undo_si_redo.py
@@ -1,9 +1,10 @@
 from Domain.obiect2 import get_new_object, get_id
-from Logic.crud import create, delete
+from Logic.crud import update
 from Logic.undo_si_redo import undo, redo, list_versions
 
 
 def get_data():
+    #returneaza o lista de obiecte
     return [
         get_new_object(1, 'birou', 'maro', 450, 'cluj'),
         get_new_object(2, 'scaun', 'lemn', 100, 'cluj'),
@@ -15,102 +16,34 @@ def get_data():
 
 
 def test_undo_redo():
-    lista = []
-    versions_list = []
-    current_version = 0
+    lista = get_data()
+    versions_list = [lista]
+    current_version = 1
+
+    #modific un obiect din lista
+
+    initial_object = get_new_object(5, 'cafetiera', 'scumpa', 470, 'iasi')
+    updated_object = get_new_object(5, 'cafetiera', 'ieftina', 200, 'iasi')
+
+    lista = update(lista, updated_object)
+
+    #lista de versiuni va fi actualizata
+
     versions_list, current_version = list_versions(versions_list, current_version, lista)
 
-    obiect1 = get_new_object(1, 'birou', 'maro', 450, 'cluj')
-    lista = create(lista, obiect1)
-    versions_list, current_version = list_versions(versions_list, current_version, lista)
+    assert initial_object not in versions_list[current_version - 1]
+    assert updated_object in versions_list[current_version - 1]
 
-    obiect2 = get_new_object(2, 'scaun', 'lemn', 100, 'cluj')
-    lista = create(lista, obiect2)
-    versions_list, current_version = list_versions(versions_list, current_version, lista)
-
-    obiect3 = get_new_object(3, 'masa', 'patrata', 330, 'arad')
-    lista = create(lista, obiect3)
-    versions_list, current_version = list_versions(versions_list, current_version, lista)
-
-    del lista[2]
-    current_version = undo(current_version)
-    assert versions_list[current_version-1] == lista
-
-    del lista[1]
-    current_version = undo(current_version)
-    assert versions_list[current_version - 1] == lista
-
-    del lista[0]
-    current_version = undo(current_version)
-    assert versions_list[current_version - 1] == lista
+    #in urma unui undo, obiectul initial se va afla in lista curenta
 
     current_version = undo(current_version)
-    assert versions_list[current_version - 1] == lista
 
-    lista = create(lista, obiect1)
-    versions_list, current_version = list_versions(versions_list, current_version, lista)
-    lista = create(lista, obiect2)
-    versions_list, current_version = list_versions(versions_list, current_version, lista)
-    lista = create(lista, obiect3)
-    versions_list, current_version = list_versions(versions_list, current_version, lista)
+    assert initial_object in versions_list[current_version - 1]
+    assert updated_object not in versions_list[current_version - 1]
+
+    #in urma unui redo, obiectul modificat se va afla in lista curenta
 
     current_version = redo(versions_list, current_version)
-    assert versions_list[current_version - 1] == lista
 
-    lista = delete(lista, get_id(obiect3))
-    current_version = undo(current_version)
-    assert versions_list[current_version - 1] == lista
-
-    lista = delete(lista, get_id(obiect2))
-    current_version = undo(current_version)
-    assert versions_list[current_version - 1] == lista
-
-    lista_verificare = lista + [obiect2]
-    current_version = redo(versions_list, current_version)
-    lista = versions_list[current_version - 1]
-    assert lista == lista_verificare
-
-    lista_verificare2 = lista_verificare + [obiect3]
-    current_version = redo(versions_list, current_version)
-    lista = versions_list[current_version - 1]
-    assert len(lista) == len(lista_verificare2)
-    assert obiect3 in lista
-
-    lista = delete(lista, get_id(obiect3))
-    current_version = undo(current_version)
-    assert versions_list[current_version - 1] == lista
-
-    lista = delete(lista, get_id(obiect2))
-    current_version = undo(current_version)
-    assert versions_list[current_version - 1] == lista
-
-    obiect4 = get_new_object(4, 'frigider', 'desc 4', 2900, 'l112')
-    lista = create(lista, obiect4)
-    versions_list, current_version = list_versions(versions_list, current_version, lista)
-
-    current_version = redo(versions_list, current_version)
-    assert versions_list[current_version - 1] == lista
-
-    lista = delete(lista, get_id(obiect4))
-    current_version = undo(current_version)
-    assert versions_list[current_version - 1] == lista
-
-    lista = delete(lista, get_id(obiect1))
-    current_version = undo(current_version)
-    assert versions_list[current_version - 1] == lista
-
-    lista_verificare = lista + [obiect1]
-    current_version = redo(versions_list, current_version)
-    lista = versions_list[current_version - 1]
-    assert lista == lista_verificare
-
-    lista_verificare = lista + [obiect4]
-    current_version = redo(versions_list, current_version)
-    lista = versions_list[current_version - 1]
-    assert lista == lista_verificare
-
-    current_version = redo(versions_list, current_version)
-    assert versions_list[current_version - 1] == lista
-
-
-test_undo_redo()
+    assert initial_object not in versions_list[current_version - 1]
+    assert updated_object in versions_list[current_version - 1]
